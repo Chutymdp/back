e222bd528e7083eafbda135477b9be76ed98d8ae
Emmanuel Cruz <emcruzre@ucq.edu.mx>
2019-12-13 22:58:28 -0600

    Commit : 9
    Corrección de puerto en el servidor para el deploy en Heroku

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/api', ApiRouter);
 
 //Configuraciones puertos 
-app.set('puerto', process.env.PORT || 4000);
+app.set('puerto', process.env.PORT || 3000);
 app.listen(app.get('puerto'), function () {
   console.log('Servidor corriendo en puerto: '+ app.get('puerto'));
 });
