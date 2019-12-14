d4e5344e6f8d0f398a1e334d58a7a8921de60729
Emmanuel Cruz <emcruzre@ucq.edu.mx>
2019-12-13 22:55:23 -0600

    Commit : 8
    Agregamos el inico de la landing al servidor

--- server/server.js
+++ server/server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const session = require('express-session');
 const ApiRouter = require('./routes');
 const Cors = require('cors');
+const path = require('path');
 
 const app = express();
 app.use(Cors());
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
 app.use('/api', ApiRouter);
 
 //Configuraciones puertos 
-app.set('puerto', process.env.PORT || 3000);
+app.set('puerto', process.env.PORT || 4000);
 app.listen(app.get('puerto'), function () {
   console.log('Servidor corriendo en puerto: '+ app.get('puerto'));
 });
